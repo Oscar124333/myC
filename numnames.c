ba3768592d646aafdd17192ec24938c5ea27136e
Oscar124333 <160776538+Oscar124333@users.noreply.github.com>
2025-05-26 07:49:07 +0000

    numbers to english translator now works

--- numnames.c
+++ numnames.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
-char *stdNumArr[3][10] = {
-    {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"},
-    {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
-    {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}};
-char *bigArr[3] = {"hundred", "thousand", "million"};
+// Constants; array identifiers & place values
+const int arrONES = 0;
+const int arrTEENS = 1;
+const int arrTENS = 2;
 
+const int THSND = 1000;
+const int MIL = 1000000;
+
+// 2D library of numbers in English
+char *numLib[3][10] = {
+    {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"},
+    {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
+    {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"},
+};
+
+// Prototypes
 int digit_counter(int n);
+void ones_huns(int n);
 
 int main(void)
 {
-
+    // Number grabber
     int num = 0;
     printf("Number: ");
     if (scanf("%i", &num) != 1)
@@ -18,49 +29,27 @@ int main(void)
         printf("Input error.\n");
         return 1;
     }
-    printf("\n");
 
     int digits = digit_counter(num);
-    printf("Digits: %i\n", digits);
 
-    if (digits == 1)
+    // Prints all millions place-values
+    if (digits >= 7)
     {
-        printf("%s\n", stdNumArr[0][num - 1]);
-    }
-    else if (num < 20)
-    {
-        printf("%s\n", stdNumArr[1][num % 10]);
-    }
-    else
-    {
-        if (digits > 2)
-        {
-            char *big = bigArr[digits - 3];
-            printf("%s ", big);
-        }
-
-        int tenVal = (num / 10) % 10;
-        int oneVal = num % 10;
-        char *tens = "";
-        if (tenVal > 0)
-        {
-            tens = stdNumArr[2][(tenVal - 2)];
-        }
-        char *ones = stdNumArr[0][oneVal - 1];
-
-        printf("%s", tens);
-        if (0)
-        {
-            printf("%s", ones);
-        }
-        else if (ones != 0)
-        {
-            printf("-%s", ones);
-        }
-        printf("\n");
+        ones_huns(num / MIL);
+        printf(" million ");
     }
 
-    printf("\n%i\n", num);
+    // Prints all thousands place-values if present
+    if (digits >= 4 && (num / THSND) % THSND > 0)
+    {
+        ones_huns(num / THSND);
+        printf(" thousand ");
+    }
+
+    // Prints 1-999
+    ones_huns(num);
+
+    printf("\n");
     return 0;
 }
 
@@ -73,4 +62,49 @@ int digit_counter(int n)
         i++;
     }
     return i;
+}
+
+void ones_huns(int n)
+{
+    int hunVal = (n / 100) % 10;
+    int tenVal = (n / 10) % 10;
+    int oneVal = n % 10;
+
+    // Prints hundreds place
+    if (hunVal > 0)
+    {
+        char *huns = numLib[arrONES][hunVal];
+        printf("%s hundred ", huns);
+    }
+
+    // Prints teens & terminates, or tens, or nothing if only ones
+    if (tenVal > 0)
+    {
+        char *tens = "";
+        if (tenVal == arrTEENS)
+        {
+            tens = numLib[arrTEENS][oneVal];
+            printf("%s", tens);
+            return;
+        }
+        else
+        {
+            tens = numLib[arrTENS][(tenVal - arrTENS)];
+            printf("%s", tens);
+        }
+    }
+
+    // Prints ones; dash if tens present
+    if (oneVal > 0)
+    {
+        char *ones = numLib[arrONES][oneVal];
+        if (tenVal > 0)
+        {
+            printf("-%s", ones);
+        }
+        else
+        {
+            printf("%s", ones);
+        }
+    }
 }
