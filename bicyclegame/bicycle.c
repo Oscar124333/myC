6051f84d7fc968e0f9a431259576f3194c872c35
Oscar124333 <160776538+Oscar124333@users.noreply.github.com>
2025-10-20 01:07:46 +0000

    major update to bicycle.c + all "menus" are now UNrecursive + structs for player and bicycle stats added + basic gameplay function implemented (just adds $2.5 per iteration) + changed screen state variable to be local for every "menu" + added function that makes `scanf()` safer, however it requires an additional external loop :( + function names make more sense + saves function brought back to superficial life!

--- bicyclegame/bicycle.c
+++ bicyclegame/bicycle.c
@@ -3,34 +3,53 @@
 #include <unistd.h>
 
 // Constants
-#define CASEEXIT 9
+#define CASE_EXIT 9
+const int RESET = 0;
 const int EXIT = 9;
 
 // Global Variables
 int userInput = 0;
-int userState = 0;
 int userSave = 0;
+int inStatus = 0;
+
+// Gameplay Data
+    // Distance is measured in miles * 10; 100 distance = 10 miles
+typedef struct {
+    int type;
+    int HP;
+    int speed; 
+    float accidentRate;
+} Bicycle;
+Bicycle bike1 = {0, 100, 10, 0.3f};
+
+typedef struct {
+    float dBal; // Dollars (standard currency)
+    int fBal;   // Favors (premium currency)
+    float dollarRate;
+    float favorRate;
+} PlayerStats;
+PlayerStats p1 = {0.0f, 0, 1.0f, 0.11f};
 
 // Gameplay Prototypes
-void gameOverview (void);
+int bike_manual(void);
 
 // Utility Prototypes
 void wait(float seconds);
 void lineBreak(void);
+int inputHandler(int *variable);
 
 // Interface Prototypes
-int menuMain_options(void);
-int menuMain_info(void);
-int menuMain_credits(void);
-bool userSaves(void);
+bool user_saves(void);
+int main_options(void);
+int main_info(void);
+int main_credits(void);
+int game_overview (void);
+void displayStats(PlayerStats player);
 
 int main(void)
 {
-    lineBreak();
-    
-    printf("Welcome to Oscar's Bicycle Game!\n");
-    printf("Please use numbers to indicate your choices.\n");
-    
+    int m_Screen = 0;
+
     enum MenuMain{
         MAIN,
         START,
@@ -39,100 +58,94 @@ int main(void)
         CREDITS
     };
     
+    lineBreak();
+    
+    printf("Welcome to Oscar's Bicycle Game!\n");
+    printf("Please use numbers to indicate your choices.\n");
+    
     do   
     {
-        
-        switch (userState)
+        switch (m_Screen)
         {
             case MAIN:
-                lineBreak();
+                do
+                {
+                    lineBreak();
+                    
+                    printf("Main Menu\n");
+                    printf("1: Start\n2: Options\n3: Info & How-To\n4: Credits\n\n9: Exit\n\n");
+                    printf("==> ");
+                    inStatus = inputHandler(&m_Screen);
+                } while (inStatus != 1);
 
-                printf("Main Menu");
-                printf("\n1: Start\n2: Options\n3: Info & How-To\n4: Credits\n9: Exit\n");
-                printf("==> ");
-                scanf("%i", &userState);
                 break;
             case START:
                 printf("you started the game! wow!\n");
-                gameOverview();
+                if (user_saves())
+                {
+                    printf("Loading save #%d.\n", userSave);
+                    m_Screen = game_overview();
+                }
                 break;
             case OPTIONS:
-                userState = menuMain_options();
+                m_Screen = main_options();
                 break;
             case INFO_HOW:
-                userState = menuMain_info();
+                m_Screen = main_info();
                 break;
             case CREDITS:
-                userState = menuMain_credits();
+                m_Screen = main_credits();
                 break;
-            case CASEEXIT:
+            case CASE_EXIT:
                 lineBreak();
                 printf("Exiting game.\n");
                 break;
             default:
-                printf("Pleae try again.\n");
-                userState = MAIN;
+                m_Screen = MAIN;
                 break;
         }
-    } while (userState != EXIT);
+    } while (m_Screen != EXIT);
 
-/*
-*   if (userInput == EXIT)
-*   {
-*       return 1;
-*   }
-*   else if (userSaves())
-*   {
-*       printf("Loading save #%i.\n", userSave);
-*       gameOverview();
-*   }
-*   return 0;
-*/
+return 0;
 }
+
 
 /******************************************/
 //           Gameplay Functions           //
 /******************************************/
 
-void gameOverview(void)
+int bike_manual(void)
 {
     lineBreak();
-    
-    char *day[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-    printf("You wake up. It's day %i. %s.\n", 1, day[1]);
+    printf("biking...\n"); // will make a automagical function that does dot dot dot for me
+    p1.dBal += 2.5f;
 
-    do
-    {
-        printf("Y\n");
-        scanf("%i", &userInput);
-
-    } while (userInput != EXIT);
-
-    if (userInput == EXIT)
-    {
-        // some auto save functionality here
-    }
+    return RESET;
 }
 
 /*******************************************/
 //           Interface Functions           //
 /*******************************************/
 
-bool userSaves(void) // Currently, '1' is the only correct choice.
+bool user_saves(void) // Currently, '1' is the only correct choice.
 {
+    lineBreak();
+
     do
     {
-        lineBreak();
-    
-        printf("Please choose your save.\n");
-        printf("==> ");
-        scanf("%i", &userSave);
+        do
+        {
+            printf("Please choose your save.\n");
+            printf("\n==> ");
+            inStatus = inputHandler(&userSave);
+
+            lineBreak();
+        } while (inStatus != 1);
         
         if (userSave == 1)
         {
-            lineBreak();
-            printf("Successfully selected save #%i.\n", userSave);
+            printf("Successfully selected save #%d.\n", userSave);
             return true;
         }
         else
@@ -140,11 +153,13 @@ bool userSaves(void) // Currently, '1' is the only correct choice.
             printf("Save not found.\n");
         }
     } while (true);
-
 }
 
-int menuMain_options(void)
+
+int main_options(void)
 {
+    int m_opScreen = 0;
+
     enum Options{
         OPTIONS,
         PLACEHOLDER
@@ -152,32 +167,37 @@ int menuMain_options(void)
 
     do
     {
-        switch (userInput)
+        switch (m_opScreen)
         {
             case OPTIONS:
-                lineBreak();
+                do
+                {
+                    lineBreak();
 
-                printf("Options\n");
-                printf("9: Exit\n");
-                printf("==> ");
-                scanf("%i", &userInput);
+                    printf("Options\n");
+                    printf("\n9: Exit\n\n");
+                    printf("==> ");
+                    inStatus = inputHandler(&m_opScreen);
+                } while (inStatus != 1);
                 break;
             case PLACEHOLDER:
-                userInput = OPTIONS;
+                m_opScreen = OPTIONS;
                 break;
-            case CASEEXIT:
+            case CASE_EXIT:
                 break;
             default:
-                printf("Please try again.\n");
-                userInput = OPTIONS;
+                m_opScreen = OPTIONS;
                 break;
         }
-    } while (userInput != EXIT);
-    return 0;
+    } while (m_opScreen != EXIT);
+
+    return RESET;
 }
 
-int menuMain_info(void)
+int main_info(void)
 {
+    int m_inScreen = 0;
+    
     enum Info{
         INFO,
         H2P,
@@ -186,71 +206,157 @@ int menuMain_info(void)
 
     do
     {
-        lineBreak();
-        printf("Information\n");
-        printf("1: How to Play\n2: Lore\n9: Exit\n");
-        printf("==> ");
-        scanf("%i", &userInput);
-    } while ((userInput < 1 || userInput > 2) && userInput != EXIT);
-    
-    if (userInput == H2P)
-    {
-        do
+        switch (m_inScreen)
         {
-            lineBreak();
-            printf("How to Play WIP\n");
-            printf("9: Exit\n");
-            printf("==> ");
-            scanf("%i", &userInput);
-        } while (userInput != EXIT);
+            case INFO: 
+                do
+                {
+                    lineBreak();
+                    
+                    printf("Information\n");
+                    printf("1: How to Play\n2: Lore\n\n9: Exit\n\n");
+                    printf("==> ");
+                    inStatus = inputHandler(&m_inScreen);
+                } while (inStatus != 1);
 
-        if (userInput == EXIT)
-        {
-            menuMain_info();
+                break;
+            case H2P:
+                do
+                {
+                    do
+                    {
+                        lineBreak();
+
+                        printf("How to Play WIP\n");
+                        printf("\n9: Exit\n\n");
+                        printf("==> ");
+                        inStatus = inputHandler(&m_inScreen);
+                    } while (inStatus != 1);
+                } while (m_inScreen != EXIT);
+
+                m_inScreen = INFO;
+                break;
+            case LORE:
+            do
+                {
+                    do
+                    {
+                        lineBreak();
+                        printf("Lore WIP\n");
+                        printf("\n9: Exit\n\n");
+                        printf("==> ");
+                        inStatus = inputHandler(&m_inScreen);
+                    } while (inStatus != 1);
+                } while (m_inScreen != EXIT);
+
+                m_inScreen = INFO;
+                break;
+            default:
+                m_inScreen = INFO;
+                break;
         }
-    }
-    else if (userInput == LORE)
-    {
-        do
-        {
-            lineBreak();
-            printf("Lore WIP\n");
-            printf("9: Exit\n");
-            printf("==> ");
-            scanf("%i", &userInput);
-        } while (userInput != EXIT);
-        
-        if (userInput == EXIT)
-        {
-            menuMain_info();
-        }
-    }
-    else if (userInput == EXIT)
-    {
-        return 0;
-    }
+    } while (m_inScreen != EXIT);
+    
+    return RESET;
 }
 
-int menuMain_credits(void)
+int main_credits(void)
 {
+    int m_crScreen = 0;
+
     enum Credits{
         CREDITS,
         PLACEHOLDER
     };
+    
+    do
+    {
+        switch (m_crScreen)
+        {
+            case CREDITS:
+                do
+                {
+                    lineBreak();
+                    
+                    printf("Credits WIP\n");
+                    printf("\n9: Exit\n\n");
+                    printf("==> ");
+                    inStatus = inputHandler(&m_crScreen);
+                } while (inStatus != 1);
+                break;
+            case PLACEHOLDER:
+                m_crScreen = CREDITS;
+                break;
+            case CASE_EXIT:
+                break;
+            default:
+                m_crScreen = CREDITS;
+                break;
+        }
+    } while (m_crScreen != EXIT);
+    
+    return RESET;
+}
+
+int game_overview(void)
+{
+    int g_ovScreen = 0;
+
+    enum game_options{
+        OVERVIEW,
+        GO2SCHOOL,
+        SHOP,
+        SKILLS,
+        ITERATE
+    };
+
+    lineBreak();
+
+    printf("You wake up. It's day %d.\n", 1);
 
     do
     {
-        lineBreak();
-        printf("Credits WIP\n");
-        printf("9: Exit\n");
-        printf("==> ");
-        scanf("%i", &userInput);
-    } while (userInput != EXIT);
-    
-    if (userInput == EXIT)
-    {
-        return 0;
-    }
+        switch (g_ovScreen)
+        {
+            case OVERVIEW:
+                do
+                {
+                    lineBreak();
+
+                    if (true)
+                    {
+                        displayStats(p1);
+                    }
+
+                    printf("Day %d.\n", 0);
+                    printf("1: Bike to School\n2: Shop\n3: Skills\n4: Iterate\n\n9: Exit\n\n");
+                    printf("==> ");
+                    inStatus = inputHandler(&g_ovScreen);
+                } while (inStatus != 1);
+                break;
+            case GO2SCHOOL:
+                g_ovScreen = bike_manual();
+                break;
+            case SHOP:
+            case SKILLS:
+            case ITERATE:
+                g_ovScreen = OVERVIEW;
+                break;
+            default:
+                g_ovScreen = OVERVIEW;
+                break;
+        }
+    } while (g_ovScreen != EXIT);
+
+    return RESET;
+}
+
+void displayStats(PlayerStats player)
+{
+    printf("Balance: $%.2lf\n", player.dBal);
+    printf("Favors:  %d\n", player.fBal);
+    printf("$ Mult:  %.2fx\n", player.dollarRate);
+    printf("F Mult:  %.2fx\n\n", player.favorRate);
 }
 
 /*****************************************/
@@ -296,4 +402,19 @@ void lineBreak(void)
     
     arr[arrSizeTotal - arrEnd] = '\0';
     printf("%s\n", arr);
+}
+
+int inputHandler(int *variable)
+{
+    int status = 0;
+    int ch;
+    status = scanf("%d", variable);
+    if (status == 1)
+    {
+        return status;
+    }
+    else
+    {
+        while ((ch = getchar()) != '\n' && ch != EOF); // Clear buffer
+    }
 }
