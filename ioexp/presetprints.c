3e785dc2ed7a9a57dc885724a4912ce7e39c4d7e
Chi Sieu Huynh <160776538+Oscar124333@users.noreply.github.com>
2026-02-04 00:04:50 -0800

    made presetprints work
    only kind of work

--- ioexp/presetprints.c
+++ ioexp/presetprints.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     char *fileLib[] = {"file1.txt", "file2.txt", "file3.txt"};
-    char *userStr;
+    char *userStr = malloc(sizeof(char) * 15);
     FILE *file_ptr;
 
     int i = 0;
@@ -14,10 +15,10 @@ int main(void)
     scanf("%d", &i);
 
     printf("Please enter some text.\n");
+    // no spaces allowed
+    scanf("%s", userStr);
 
-    scanf("%s", &*userStr);
-
-    file_ptr = fopen(fileLib[i], "w");
+    file_ptr = fopen(fileLib[i - 1], "a");
     fprintf(file_ptr, "%s\n", userStr);
     fclose(file_ptr);
     return 0;
