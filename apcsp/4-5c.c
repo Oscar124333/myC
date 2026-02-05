849304bac11015320d5f556923237268da276b19
Chi Sieu Huynh <160776538+Oscar124333@users.noreply.github.com>
2026-02-05 18:17:22 +0000

    start 4-5c + also removed stdlib.h from 4-5b

---
+++ apcsp/4-5c.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int integerInputHandler(int *variable);
+
+int main(void)
+{
+    // Problem 1, Append and Extend with names
+    int nameAmount = 0;
+    integerInputHandler(&nameAmount);
+    char **nameArr = malloc(sizeof(char) * nameAmount);
+    
+    char buffer[25]
+
+    return 0;
+}
+
+int integerInputHandler(int *variable)
+{
+    int status = 0;
+    int ch;
+    status = scanf("%d", variable);
+    if (status != 1)
+    {
+        while ((ch = getchar()) != '\n' && ch != EOF); // Clear buffer
+    }
+    return status;
+}
+int stringInputHandler(char *variable)
+{
+    int status = 0;
+    int ch;
+    status = scanf("%s", variable);
+    if (status != 1)
+    {
+        while ((ch = getchar()) != '\n' && ch != EOF); // Clear buffer
+    }
+    return status;
+}
