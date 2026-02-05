e5de475a2fe0e6c14b549987e23f0747ca442d12
Chi Sieu Huynh <160776538+Oscar124333@users.noreply.github.com>
2026-02-05 17:59:18 +0000

    finished 4-5b.c

--- apcsp/4-5b.c
+++ apcsp/4-5b.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int maximum(int integerList[], int integerListLength);
 int minimum(int integerList[], int integerListLength);
@@ -23,28 +24,74 @@ int main(void)
     char outputBuffer[40];
     for (int i = 0; i < listAmount; i++)
     {
-        sprintf(outputBuffer, "Example #%i \n  Max: %i \n  Min: %i", i + 1, maximum(intListArr[i], intListLengths[i]), minimum(intListArr[i], intListLengths[i]));
+        sprintf(outputBuffer, "\nExample #%i \n  Max: %i \n  Min: %i", i + 1, maximum(intListArr[i], intListLengths[i]), minimum(intListArr[i], intListLengths[i]));
         printf("%s\n", outputBuffer);
     }
 
+
+    printf("\n********************\n\n");
+
+
     // Problem 2, owls
-    char *sampleText =
-        "I really like owls. Did you know that an owl's eyes are more than "
-        "twice as big as the eyes of other birds of comparable weight? And "
-        "that when an owl partially closes its eyes during the day, it is "
-        "just blocking out light? Sometimes I wish I could be an owl.";
-
-    int textSize = (int)(sizeof(sampleText)/sizeof(char *));
+    char sampleText2[] =
+    "I really like owls. Did you know that an owl's eyes are more than "
+    "twice as big as the eyes of other birds of comparable weight? And "
+    "that when an owl partially closes its eyes during the day, it is "
+    "just blocking out light? Sometimes I wish I could be an owl.";
+    
+    int textSize2 = (int)(strlen(sampleText2));
     int length_of_the_word_owl = 3;
-
-    for (int i = 0, char currentText[3]; i < textSize - length_of_the_word_owl; i++)
+    int owlCount = 0;
+    
+    for (int i = 0; i < textSize2 - length_of_the_word_owl; i++)
     {
+        char owl[] = "owl";
+        char currentText[4] = {0};
+        
         for (int j = 0; j < length_of_the_word_owl; j++)
         {
-            sprintf(currentText, )
+            currentText[j] = sampleText2[i + j];
         }
         
+        if (strcmp(currentText, owl) == 0)
+        {
+            owlCount++;
+        }
     }
+    
+    printf("Number of owls: %i\n", owlCount);
+    
+
+    printf("\n********************\n\n");
+
+
+    // Problem 3, edgy I (lowercase i only)
+    //  Note: will not do string user input
+    char sampleText3[] =
+        "Indescribable oppression, which seemed to generate in some " 
+        "unfamiliar part of her consciousness, filled her whole being "
+        "with a vague anguish. It was like a shadow, like a mist "
+        "passing across her soul's summer day. It was strange and "
+        "unfamiliar; it was a mood. She did not sit there inwardly "
+        "upbraiding her husband, lamenting at Fate, which had directed "
+        "her footsteps to the path which they had taken. She was just "
+        "having a good cry all to herself. The mosquitoes made merry "
+        "over her, biting her firm, round arms and nipping at her bare "
+        "insteps. Mississipipipipipissipipiii.";
+
+    int textSize3 = strlen(sampleText3);
+
+    for (int i = 0; i < textSize3; i++)
+    {
+        if (sampleText3[i] == 'i')
+        {
+            sampleText3[i] = '!';
+        }
+    }
+
+    printf("%s\n", sampleText3);
+
+
 
     return 0;
 }
@@ -52,7 +99,7 @@ int main(void)
 int maximum(int integerList[], int integerListLength)
 {
     int max = integerList[0];
-
+    
     for (int i = 1; i < integerListLength; i++)
     {
         if (integerList[i] > max)
